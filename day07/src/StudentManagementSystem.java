0c9bbce86f3d836e377d869d9161bbc79fab4236
Anurag <anuragsingh98352@gmail.com>
2026-08-28 03:51:09 +0530

    Day 7: Student Management System + Week 1 revision

---
+++ day07/src/StudentManagementSystem.java
@@ -0,0 +1,21 @@
+public class StudentManagmentSystem {
+
+    class Student {
+
+        private String name;
+        private int roll;
+        private int marks;
+        private int admissionYear;
+
+        Student(String name, int roll, int marks, int admissionYear) {
+            this.name = name;
+            this.roll = roll;
+            this.marks = marks;
+            this.admissionYear = admissionYear;
+        }
+    }
+
+
+
+
+}
