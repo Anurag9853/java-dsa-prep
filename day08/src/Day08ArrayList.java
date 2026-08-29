e393b3a737d03d891753799dfee1f0478fd6c35f
Anurag <anuragsingh98352@gmail.com>
2026-08-29 22:54:44 +0530

    Day 8: Collections - List and Set, 4 DSA problems

---
+++ day08/src/Day08ArrayList.java
@@ -0,0 +1,19 @@
+//Build a complete Java program that:
+//
+//Creates an ArrayList<String>.
+//Adds at least 4 names.
+//Removes one name.
+//Checks whether a particular name exists using contains().
+//Iterates through the final list and prints each name.
+
+import java.util.ArrayList;
+
+public class ArrayList {
+
+    public static void main(String[] args) {
+
+
+    }
+
+
+}
